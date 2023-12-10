04619b9693aa1ad6d0e4d5f1336a100ffffb0b9b
wonseok <dnjstjr245@gmail.com>
2023-12-10 09:58:37 +0900

    Apply spotless plugin to all projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+import com.diffplug.gradle.spotless.SpotlessExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("root.publication")
     //trick: for the same plugin versions in all sub-modules
@@ -6,5 +11,25 @@ plugins {
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.composeMultiplatform).apply(false)
+    alias(libs.plugins.spotless).apply(false)
 }
 
+allprojects {
+    apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
+    configure<SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("**/build/")
+            ktlint(libs.versions.ktlint.get())
+        }
+        kotlinGradle {
+            target("**/*.gradle.kts")
+            targetExclude("**/build/")
+            ktlint(libs.versions.ktlint.get())
+        }
+    }
+
+    tasks.withType<KotlinCompile>().all {
+        kotlinOptions { freeCompilerArgs += "-Xexpect-actual-classes" }
+    }
+}
