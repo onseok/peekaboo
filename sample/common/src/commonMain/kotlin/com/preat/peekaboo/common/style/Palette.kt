92dd2831b232828d0b50893319548972315aeae0
wonseok <dnjstjr245@gmail.com>
2023-12-17 18:35:08 +0900

    Added permissionDeniedContent usage example in sample app

--- sample/common/src/commonMain/kotlin/com/preat/peekaboo/common/style/Palette.kt
+++ sample/common/src/commonMain/kotlin/com/preat/peekaboo/common/style/Palette.kt
@@ -14,6 +14,10 @@ object PeekabooColors {
     val uiLightBlack = Color(25, 25, 28).copy(alpha = 0.7f)
     val darkBackground = Color(0xFF19191C)
     val darkOnBackground = Color(0xFFFFFFFF)
+    val primary = Color(0xFFE0E0E0)
+    val onPrimary = Color(0xFF000000)
+    val darkPrimary = Color(0xFF424242)
+    val darkOnPrimary = Color(0xFFFFFFFF)
 }
 
 @Suppress("FunctionName")
@@ -25,11 +29,15 @@ fun PeekabooTheme(content: @Composable () -> Unit) {
             MaterialTheme.colors.copy(
                 background = PeekabooColors.darkBackground,
                 onBackground = PeekabooColors.darkOnBackground,
+                primary = PeekabooColors.darkPrimary,
+                onPrimary = PeekabooColors.darkOnPrimary,
             )
         } else {
             MaterialTheme.colors.copy(
                 background = PeekabooColors.background,
                 onBackground = PeekabooColors.onBackground,
+                primary = PeekabooColors.primary,
+                onPrimary = PeekabooColors.onPrimary,
             )
         }
 
