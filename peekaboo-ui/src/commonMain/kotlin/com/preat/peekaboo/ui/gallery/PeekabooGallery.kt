2ac95c338d99b780ba3dd507efe8ed0ce1e8bb7f
wonseok <dnjstjr245@gmail.com>
2024-02-05 15:20:24 +0900

    Implement GalleryPickerState and PeekabooGallery components, add media photo fetching logic, and update manifest with necessary permissions

---
+++ peekaboo-ui/src/commonMain/kotlin/com/preat/peekaboo/ui/gallery/PeekabooGallery.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 onseok
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.preat.peekaboo.ui.gallery
+
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Suppress("FunctionName")
+@Composable
+expect fun PeekabooGallery(
+    modifier: Modifier = Modifier,
+    state: GalleryPickerState = rememberGalleryPickerState(),
+    lazyGridState: LazyGridState = rememberLazyGridState(),
+    backgroundColor: Color = Color.Black,
+    header: @Composable () -> Unit = {},
+    permissionDeniedContent: @Composable () -> Unit = {},
+    onImageSelected: (ByteArray?) -> Unit,
+)
