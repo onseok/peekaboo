84b028e87f75d42e05d30dec81b4d66b897fa1cd
wonseok <dnjstjr245@gmail.com>
2024-02-05 15:22:26 +0900

    Refactor camera-related classes into a dedicated 'camera' package

--- peekaboo-ui/src/commonMain/kotlin/com/preat/peekaboo/ui/camera/CameraMode.kt
+++ peekaboo-ui/src/commonMain/kotlin/com/preat/peekaboo/ui/camera/CameraMode.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.preat.peekaboo.ui
+package com.preat.peekaboo.ui.camera
 
 /**
  * Represents the camera modes available in the `PeekabooCamera` composable.
