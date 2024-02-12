2b2a6e489996a1acd8d3772ca74a19991994fe2a
onseok <dnjstjr245@gmail.com>
2024-02-12 15:47:26 +0900

    Remove unused imports

--- peekaboo-ui/src/androidMain/kotlin/com/preat/peekaboo/ui/gallery/PeekabooGallery.android.kt
+++ peekaboo-ui/src/androidMain/kotlin/com/preat/peekaboo/ui/gallery/PeekabooGallery.android.kt
@@ -38,8 +38,6 @@ import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
