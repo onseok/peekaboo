5ee212b0490c95ae0c776732371a951b0e7510e3
SuLG-k <nenashkinvov@gmail.com>
2024-03-01 00:19:47 +0700

    Add docs for PeekabooCameraState

--- peekaboo-ui/src/commonMain/kotlin/com/preat/peekaboo/ui/camera/PeekabooCameraState.kt
+++ peekaboo-ui/src/commonMain/kotlin/com/preat/peekaboo/ui/camera/PeekabooCameraState.kt
@@ -1,14 +1,24 @@
 package com.preat.peekaboo.ui.camera
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 
 
+/**
+ * Create and [remember] a [PeekabooCameraState]
+ * @param initialCameraMode The initial camera mode (front or back). Default is [CameraMode.Back]. Changes does not affect state. To toggle use [PeekabooCameraState.toggleCamera]
+ * @param onCapture A lambda called when a photo is captured, providing the photo as a ByteArray or null if the capture fails.
+ */
 @Composable
 expect fun rememberPeekabooCameraState(
     initialCameraMode: CameraMode = CameraMode.Back,
     onCapture: (ByteArray?) -> Unit,
 ): PeekabooCameraState
 
+
+/**
+ * State of [PeekabooCamera]. Contains states relating to camera control.
+ */
 expect class PeekabooCameraState {
 
     var isCameraReady: Boolean
