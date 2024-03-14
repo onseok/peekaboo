781981e027ebe6d5da831c28fddbc10d842634e2
onseok <dnjstjr245@gmail.com>
2024-03-14 18:59:10 +0900

    Fix handling of Exif orientation info for image rotation/flipping

--- peekaboo-ui/src/androidMain/kotlin/com/preat/peekaboo/ui/gallery/PeekabooGallery.android.kt
+++ peekaboo-ui/src/androidMain/kotlin/com/preat/peekaboo/ui/gallery/PeekabooGallery.android.kt
@@ -183,6 +183,19 @@ private fun rotateImageIfRequired(
         ExifInterface.ORIENTATION_ROTATE_90 -> matrix.postRotate(90f)
         ExifInterface.ORIENTATION_ROTATE_180 -> matrix.postRotate(180f)
         ExifInterface.ORIENTATION_ROTATE_270 -> matrix.postRotate(270f)
+        ExifInterface.ORIENTATION_FLIP_HORIZONTAL -> matrix.preScale(-1.0f, 1.0f)
+        ExifInterface.ORIENTATION_FLIP_VERTICAL -> {
+            matrix.preScale(1.0f, -1.0f)
+            matrix.postRotate(180f)
+        }
+        ExifInterface.ORIENTATION_TRANSPOSE -> {
+            matrix.preScale(-1.0f, 1.0f)
+            matrix.postRotate(270f)
+        }
+        ExifInterface.ORIENTATION_TRANSVERSE -> {
+            matrix.preScale(-1.0f, 1.0f)
+            matrix.postRotate(90f)
+        }
     }
 
     return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
